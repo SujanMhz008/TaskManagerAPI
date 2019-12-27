50d3b7b40968898b88ccff6fd3ace7157d7c5071
Sujan Maharjan <zexcu008@gmail.com>
2019-12-27 11:24:36 +0545

    SOme works done

--- app/src/main/java/com/sujanmaharjan008/taskmanagerapi/url/Url.java
+++ app/src/main/java/com/sujanmaharjan008/taskmanagerapi/url/Url.java
@@ -4,8 +4,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Url {
-    public static final String BASE_URL = "http://10.0.2.2:3000";
-//    public static final String BASE_URL = "http://172.100.100.5:3000";
+    public static final String BASE_URL = "http://10.0.2.2:3000/";
+//    public static final String BASE_URL = "http://172.100.100.5:3000/";
 
     public static String token = "";
 
